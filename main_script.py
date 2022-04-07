7c9259c71125c6e845a41d0655f22ae648fe03b3
abdhulkhadhir <69329813+abdhulkhadhir@users.noreply.github.com>
2022-04-07 14:46:50 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -9,35 +9,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-plt.style.use('ggplot')
 
 #Title
-st.title('AKspense Tracker')
+st.title('AKespense Tracker')
 
 link = 'https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3gvcyFBdWZ6bnAweHVfSG5nZlpGaXg4OFZ5azdvMFlZcnc_ZT10WnBwRDA/root/content'
 
-# # Load Uber Data
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-# @st.cache
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-# data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
+
 data = pd.read_excel(link,'Transactions', engine='openpyxl')
 data['Month'] = data['Date'].dt.strftime('%b-%Y')
 
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Loading data -DONE!")
 
 # Print Raw data as a DataFrame
 
@@ -73,7 +59,6 @@ if show_trans:
         
 # Select Occasion
 
-
 if 'All' in occasion:
     data_filtered = data
 else:
@@ -89,7 +74,6 @@ table_new['Month'] = pd.to_datetime(table_new['Month'])
 
 
 fig = px.bar(table_new, x= 'Month', y=list(table_new.columns))
-#fig = px.bar(data_filtered, x='Month', y="Amount", color = 'Category')
 
 st.plotly_chart(fig, use_container_width=True)
 
@@ -97,8 +81,3 @@ st.plotly_chart(fig, use_container_width=True)
 if show_table:
     st.subheader('Monthly Expenses Summary Table')
     st.write(table['Amount'])
-        
-
-
-
-
