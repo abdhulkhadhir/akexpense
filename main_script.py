6eb7e7d94102363a7ec20640c218c1425b2ec422
abdhulkhadhir <69329813+abdhulkhadhir@users.noreply.github.com>
2022-04-07 15:06:31 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -71,7 +71,7 @@ st.subheader('Monthly Expenses Summary Plot')
 # Monthly Expenses - Summary plot
 table = pd.pivot_table(data = data_filtered, index = ['Month'], columns = 'Category', values = 'Amount',
                        aggfunc=np.sum, margins = True)
-table_new = table['Amount'].reset_index()
+table_new = table.reset_index()
 table_new['Month'] = pd.to_datetime(table_new['Month'])
 
 
